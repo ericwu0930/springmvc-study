a93deb85b2eea971fc019fa8017bf63780c44421
erichwu <erichwu@tencent.com>
2020-05-03 14:27:39 +0800

    使用xml配置springmvc

--- springmvc-02-hellomvc/src/main/java/io/github/ericwu0930/controller/HelloController.java
+++ springmvc-02-hellomvc/src/main/java/io/github/ericwu0930/controller/HelloController.java
@@ -1,28 +1,23 @@
 package io.github.ericwu0930.controller;
 
-
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 /**
  * @author erichwu
  * @date 2020/5/3
  */
 public class HelloController implements Controller {
-
-    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        //ModelAndView 模型和视图
-        ModelAndView mv = new ModelAndView();
-
-        //封装对象，放在ModelAndView中。Model
-        mv.addObject("msg","HelloSpringMVC!");
-        //封装要跳转的视图，放在ModelAndView中
-        mv.setViewName("hello"); //: /WEB-INF/jsp/hello.jsp
-        return mv;
+    public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
+        ModelAndView modelAndView = new ModelAndView();
+        //业务代码
+        String res="Hello,SpringMVC";
+        modelAndView.addObject("msg",res);
+        //视图跳转
+        modelAndView.setViewName("test");
+        return modelAndView;
     }
-
 }
